6456ad32b61137590fccb31876ac9c1af43a5a9a
Noelia <noelia.flores@training.assuresoft.com>
2023-12-05 11:02:00 -0400

    POST Create account complete with helpers

--- src/main/java/Helpers.java
+++ src/main/java/Helpers.java
@@ -1,2 +1,28 @@
 public class Helpers {
+    static String username = "LuisOropeza";
+    static String password = "JtEPshHpXEi_jPu@854";
+    static String userId;
+    static String token;
+    public static String getBaseUrl() {
+        return "https://bookstore.toolsqa.com";
+    }
+
+    public static String getAccountUrl() {
+        return getBaseUrl() + "/Account/v1/User";
+    }
+
+    public static void setUserID(String userIdFromRequest){
+        userId = userIdFromRequest;
+    }
+
+    public static String getUserID(){
+        return userId;
+    }
+    public static String getUsername(){
+        return username;
+    }
+
+    public static String getPassword(){
+        return password;
+    }
 }
--- src/test/java/CRUDAccountModule.java
+++ src/test/java/CRUDAccountModule.java
@@ -1,2 +1,33 @@
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
 public class CRUDAccountModule {
+
+    @Test
+    void createAccount(){
+        String postUrl = Helpers.getAccountUrl();
+
+        JSONObject requestBody = new JSONObject()
+                .put("userName", Helpers.getUsername())
+                .put("password", Helpers.getPassword());
+
+        Response response = given()
+                                .header("Accept", "application/json")
+                                .header("Content-Type", "application/json")
+                                .body(requestBody.toString())
+                            .when()
+                                .post(postUrl)
+                            .then()
+                                .statusCode(201)
+                                .log().all()
+                                .extract()
+                                .response();
+
+        String userId = response.jsonPath().getString("userID");
+        Helpers.setUserID(userId);
+        System.out.println(userId);
+    }
 }
